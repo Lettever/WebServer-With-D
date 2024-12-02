3bd7e1165a5f9caec644a826d2ed52a95047667f
Lettever <61094520+Lettever@users.noreply.github.com>
2024-12-02 22:24:49 +0000

    Added some example of what a route consists of

--- src/router/router.d
+++ src/router/router.d
@@ -5,11 +5,22 @@ import std.typecons;
 
 alias HTTP_req = string;
 alias Route = string;
-alias Render_Fn = string function();
+alias RenderFn = string function();
+
+/*
+Route example:
+    - /index/foo
+    - /user/:id
+    the id can be anything like
+    /user/10
+    /user/bye
+    /user/1_1
+    ...
+*/
 
 class Router {
-    Render_Fn[Route][HTTP_req] routes;
-    void add(HTTP_req req, Route route, Render_Fn fn) {
+    RenderFn[Route][HTTP_req] routes;
+    void add(HTTP_req req, Route route, RenderFn fn) {
         HTTP_req a = req.toUpper();
         if(a in routes && route in routes[a]) {
             writefln("overiding function (%s, %s)", a, route);
